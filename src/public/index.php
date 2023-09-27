6f2e26a0d9d7e408c164b196fb4c0ed127597dd9
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-09-27 19:44:56 +0700

    feat: menambah routing system dasar

--- src/public/index.php
+++ src/public/index.php
@@ -2,12 +2,24 @@
 
 require_once __DIR__ . '/../app/config/bootstrap.php';
 
+$uri = $_SERVER["REQUEST_URI"];
+
+switch ($uri) {
+  case "":
+  case "/":
+    break;
+  default:
+    http_response_code(404);
+    require __DIR__ . '/../app/views/errors.php';
+    die();
+}
+
 // Test DB Connection
 try {
-    $db = new Database();
-    print_r($db->fetchAll("SELECT kode_fakultas FROM program_studi"));
-    echo "Database connection successful!";
+  $db = new Database();
+  print_r($db->fetchAll("SELECT kode_fakultas FROM program_studi"));
+  echo "Database connection successful!";
 } catch (Exception $e) {
-    echo "Database connection failed: " . $e->getMessage();
-    exit;
+  echo "Database connection failed: " . $e->getMessage();
+  exit;
 }
