8afdaf2ed0362ea3f61b0a9f796fd97de1d28739
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-10-03 20:45:08 +0700

    feat: optional database parameter in Model constructor

--- src/app/models/Model.php
+++ src/app/models/Model.php
@@ -3,7 +3,7 @@
 class Model {
     protected $db;
 
-    function __construct() {
-        $this->db = new Database();    
+    function __construct($db = null) {
+        $this->db = $db ?? new Database();
     }
 }
